114b294ddb3c59665750e3724d51ad3fedc2d2a9
Christian Oder <myself5@carbonrom.org>
2016-09-30 23:53:04 +0200

    camera: only build for sony devices

--- Android.mk
+++ Android.mk
@@ -1,42 +1,44 @@
-LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_BOARD_AUTO),true)
+  LOCAL_PATH := $(call my-dir)
 
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
-include $(CLEAR_VARS)
+  # HAL module implemenation stored in
+  # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+  include $(CLEAR_VARS)
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
+  LOCAL_MODULE_RELATIVE_PATH := hw
+  LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+  LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 
-# Include target-specific files.
-ifneq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += power-8916.c
-endif
+  # Include target-specific files.
+  ifneq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SRC_FILES += power-8916.c
+  endif
 
-ifneq ($(filter msm8952,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += power-8952.c
-endif
+  ifneq ($(filter msm8952,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SRC_FILES += power-8952.c
+  endif
 
-ifneq ($(filter msm8974,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += power-8974.c
-endif
+  ifneq ($(filter msm8974,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SRC_FILES += power-8974.c
+  endif
 
-ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += power-8994.c
-endif
+  ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SRC_FILES += power-8994.c
+  endif
 
-ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += power-8996.c
-endif
+  ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SRC_FILES += power-8996.c
+  endif
 
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+  ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
-endif
+  endif
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+  ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
+  endif
 
-LOCAL_MODULE := power.$(TARGET_DEVICE)
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
+  LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
+  LOCAL_MODULE_TAGS := optional
+  include $(BUILD_SHARED_LIBRARY)
+endif
